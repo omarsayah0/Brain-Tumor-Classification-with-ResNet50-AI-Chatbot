f10879fe7b19db6febbd77453e2d138400c52d7c
omar alethamat <omaralethamat@gmail.com>
2025-08-02 00:48:42 +0300

    Update Qynerva2.py

--- Qynerva2.py
+++ Qynerva2.py
@@ -103,7 +103,7 @@ def load_model():
         with open(MODEL_FILENAME, "wb") as f:
             for chunk in r.iter_content(chunk_size=8192):
                 f.write(chunk)
-    return keras.models.load_model(MODEL_FILENAME, compile=False)
+    #return keras.models.load_model(MODEL_FILENAME, compile=False)
 
 
 def main():
@@ -209,3 +209,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
